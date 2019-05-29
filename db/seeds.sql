456b4ee2ce1fc5ee63be46cae1a9503d567ab040
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-05-28 21:26:41 -0400

    creating files

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO burgers (burger_name,devoured)
+VALUES ("Spicy Veronica",true);
+
+INSERT INTO burgers (burger_name,devoured)
+VALUES ("Dirty Bird",false);
+
+INSERT INTO burgers (burger_name,devoured)
+VALUES ("Waffle Burger",true);
+
+INSERT INTO burgers (burger_name,devoured)
+VALUES ("Glazed Donut",true);
+
+INSERT INTO burgers (burger_name,devoured)
+VALUES ("Plain Jane", false);
