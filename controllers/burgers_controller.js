af19b0dcaba89d7c49aa81f7dee7cf03af88f0d7
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-05-31 23:19:51 -0400

    all backend should be working now

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,30 +1,40 @@
 var express = require("express");
-var burgers = require("../models/burger");
+var burger = require("../models/burger.js");
 var router = express.Router();
 
-// Get burgers from database
-router.get("/", function(req, res) {
-  connection.query("SELECT * FROM burgers;", function(err, data) {
-    if (err) throw err;
 
-    res.render("index.handlebars", { burgers: data });
+router.get('/', function(req, res) {
+  res.redirect('/burgers');
+});
+
+// Get burgers from database
+router.get("/burgers", function(req, res) {
+  burger.selectAll(function(burger_data){
+    res.render("index.handlebars", { burger_data });
   });
 });
 
 // Post create new burgers
-router.post("/api/burgers", function(req, res) {
-  burgers.create(
-    ["burger_name", devoured],
-    [req.body.burger_name, req.body.devoured],
-    function(result) {
-      if (err) throw err;
-      res.json({ id: result.insertID });
-    }
-  );
+router.post("/burgers/create", function(req, res) {
+  if (req.body.burger_name == '') {
+      console.log('No burger name entered');
+      res.redirect('/');
+  } else {
+      //takes the request object using it as input for buger.addBurger
+      burger.insertOne(req.body.burger_name, function(result) {
+          //wrapper for orm.js that using MySQL insert callback will return a log to console, render back to index with handle
+          console.log(result);
+          res.redirect('/');
+      });
+  }
 });
 
-//update burger
-router.put("/api/burgers/:id")
-
-
+//put route -> back to index
+router.put('/burgers/update', function(req, res) {
+  burger.updateOne(req.body.burger_id, function(result) {
+      //wrapper for orm.js that using MySQL update callback will return a log to console, render back to index with handle
+      console.log(result);
+      res.redirect('/');
+  });
+});
 module.exports = router;
