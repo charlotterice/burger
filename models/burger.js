bbca61102ee1c869cdba04edd4a9840ce22f3da9
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-05-31 22:51:09 -0400

    updated burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,18 @@
 var orm = require("../config/orm.js");
 
 // Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
+
+var burger = {
+    selectAll:function(cb){
+        orm.selectAll("burgers",function(res){
+            cb(res);
+        });
+    },
+    insertOne:function(name,cb){
+        orm.insertOne("burgers",["burger_name","devoured"],[name,false],cb);
+    },
+    updateOne:function(id,cb){
+        orm.updateOne("burgers",{devoured:true},condition,cb)
+    }
+};
 module.exports = burger;
