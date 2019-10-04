bdce47db1d72314541d2229333a4cbccf84ba386
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-10-03 21:59:46 -0400

    new photos folder commenting out possibly unneeded code

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,30 +1,35 @@
-$(function(){
-  $(".devoured").on("click", function(event){
-      var id = $(this).data("id");
-      var status = {devoured:true};
+$(document).ready(function(){
+    $(".create-form").on("submit", function(event) {
+        event.preventDefault();
+        console.log("submit form");
+        console.log($("#bur").val().trim());
+        var newBurger = {
+            name: $("#bur").val().trim()
+        };
+        $.ajax("/api/burgers",{
+            type: "POST",
+            data: newBurger
+        }).then(function(){
+            location.reload();
+            console.log(newBurger);
+        });
+      });   
+})
 
-      $.ajax("/api/burgers/" + id,{
-          type: "PUT",
-          data: status
-      }).then(function(){
-          location.reload();
-      });
-  });
+
+// $(function(){
+//   $(".devoured").on("click", function(event){
+//       var id = $(this).data("id");
+//       var status = {devoured:true};
+
+//       $.ajax("/api/burgers/" + id,{
+//           type: "PUT",
+//           data: status
+//       }).then(function(){
+//           location.reload();
+//       });
+//   });
 
 
-$(".create-form").on("submit", function(event) {
-  event.preventDefault();
-  console.log("submit form");
-  console.log($("#bur").val().trim());
-  var newBurger = {
-      name: $("#bur").val().trim()
-  };
-  $.ajax("/api/burgers",{
-      type: "POST",
-      data: newBurger
-  }).then(function(){
-      location.reload();
-      console.log(newBurger);
-  });
-});
-});
+
+// });
