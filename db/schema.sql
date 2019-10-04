1fb1e912c0c5991b4a05791d07bceb52887e0df9
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-10-03 21:55:12 -0400

    committing changes

--- db/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE burgers_db;
-USE burgers_db;
-CREATE TABLE burgers (
-    id INT NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR (100),
-    devoured boolean NOT NULL,
-    PRIMARY KEY (id)
-)
--- models/burger.js
+++ models/burger.js
@@ -1,22 +1,10 @@
-var orm = require("../config/orm.js");
-
-// Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
-
-var burger = {
-    selectAll:function(cb){
-        orm.selectAll("burgers",function(res){
-            cb(res);
-        });
-    },
-    insertOne:function(cols,vals,cb){
-        orm.insertOne("burgers",cols,vals,function(res){
-            cb(res);
-        });
-    },
-    updateOne:function(keyValue, condition, cb){
-        orm.updateOne("burgers",keyValue,condition,function(res){
-            cb(res);
-        })
-    }
+module.exports = function(sequelize, DataTypes){
+    var Burger= sequelize.define("Burger",{
+        burger_name:DataTypes.STRING,
+        devoured: {
+            type:DataTypes.BOOLEAN,
+            defaultValue:false
+        }
+    });
+    return Burger;
 };
-module.exports = burger;
